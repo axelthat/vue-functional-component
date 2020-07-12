9b2b40ad9c7aff4db0074cdd85dd4ed0d86fc364
Sanjay <sagarbanjade9857020528@gmail.com>
2020-07-12 19:45:54 +0545

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <function-comp />
+</template>
+
+<script>
+import FunctionComp from "./func-comp";
+
+export default {
+    name: "App",
+    components: {
+        FunctionComp
+    }
+};
+</script>
---
+++ src/func-comp.ts
@@ -0,0 +1,23 @@
+import { FunctionalComponent, h } from "vue";
+
+const FunctionComp: FunctionalComponent = (
+  props: { active: boolean },
+  { slots }
+) => {
+  const { active } = props;
+  return h("div", {
+    class: ["func-comp", { active }],
+  });
+};
+
+FunctionComp.props = {
+  active: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+};
+
+FunctionComp.displayName = "FunctionComp";
+
+export default FunctionComp;
